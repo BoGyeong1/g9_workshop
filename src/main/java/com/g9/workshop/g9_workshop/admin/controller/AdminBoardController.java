105d0423efe8e1cf5b5f1c61a6975834ff2635aa
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2023-03-04 03:05:20 +0900

    Board

---
+++ src/main/java/com/g9/workshop/g9_workshop/admin/controller/AdminBoardController.java
@@ -0,0 +1,19 @@
+package com.g9.workshop.g9_workshop.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/board")
+public class AdminBoardController {
+    
+
+    @GetMapping({""})
+    public ModelAndView boardMain(ModelAndView modelAndView) {
+        modelAndView.setViewName("admin/web/board");
+        return modelAndView;
+
+    }
+}
---
+++ src/main/java/com/g9/workshop/g9_workshop/user/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.g9.workshop.g9_workshop.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/board")
+
+public class BoardController {
+    
+}
