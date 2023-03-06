98b0b2d6ab19bd2b215ffad3e59f4e31c2367060
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2023-03-07 08:16:02 +0900

    board

--- src/main/java/com/g9/workshop/g9_workshop/user/controller/BoardController.java
+++ src/main/java/com/g9/workshop/g9_workshop/user/controller/BoardController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.g9.workshop.g9_workshop.user.dao.SharedDao;
 import com.g9.workshop.g9_workshop.user.service.BoardService;
 
 @Controller
