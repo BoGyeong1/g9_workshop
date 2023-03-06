88f0b6c8e9944399bffdcc271b8581d196b55585
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2023-03-07 08:44:07 +0900

    MAPPER

--- src/main/java/com/g9/workshop/g9_workshop/user/controller/BoardController.java
+++ src/main/java/com/g9/workshop/g9_workshop/user/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,10 +29,12 @@ public class BoardController {
         return modelAndView;
     }
 
-    @GetMapping("/view")
-    public ModelAndView boardview(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
-        Object resultMap = boardService.selectBoard(params);
-        modelAndView.addObject("resultMap", resultMap);
+    @GetMapping("/view/{postUid}")
+    public ModelAndView boardview(@RequestParam Map<String, Object> params, @PathVariable String postUid,
+            ModelAndView modelAndView) {
+        params.put("POST_UID", postUid);
+        Object boardList = boardService.getOne(params);
+        modelAndView.addObject("boardList", boardList);
         modelAndView.setViewName("user/board/boardView");
         return modelAndView;
     }
--- src/main/java/com/g9/workshop/g9_workshop/user/service/BoardService.java
+++ src/main/java/com/g9/workshop/g9_workshop/user/service/BoardService.java
@@ -3,6 +3,7 @@ package com.g9.workshop.g9_workshop.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.g9.workshop.g9_workshop.admin.dao.AdminDao;
 import com.g9.workshop.g9_workshop.user.dao.SharedDao;
 
 @Service
@@ -10,7 +11,10 @@ public class BoardService {
 
     @Autowired
     SharedDao sharedDao;
-
+    
+    @Autowired
+    AdminDao adminDao;
+    
     public Object selectBoard(Object dataMap) {
         String sqlMapId = "BoardMapper.selectBoard";
         Object result = sharedDao.getList(sqlMapId);
@@ -20,6 +24,18 @@ public class BoardService {
     public Object getBoardList() {
         String sqlMapId = "BoardMapper.selectBoardLIst";
         Object result = sharedDao.getList(sqlMapId);
+        return result;
+    }
+    
+    public Object getBoardUid(Object dataMap) {
+        String sqlMapId = "BoardMapper.selectBoardUID ";
+        Object result = sharedDao.getList(sqlMapId);
+        return result;
+    }
+    
+    public Object getOne(Object dataMap) {
+        String sqlMapId = "BoardMapper.selectBoardUID";
+        Object result = adminDao.getOne(sqlMapId, dataMap);
         return result;
     }
 }
